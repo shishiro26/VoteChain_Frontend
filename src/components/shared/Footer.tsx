be5819cb29d23612616eb95fd7f3008e5957590a
shishiro26 <bannu1302@gmail.com>
2025-05-27 14:19:38 +0530

    small fix

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,10 +1,13 @@
 // import React from "react";
 
+import { Link } from "react-router";
+
 const Footer = () => {
   return (
     <footer className="py-6 border-t">
       <div className="container mx-auto px-4 text-center text-sm text-muted-foreground">
-        © {new Date().getFullYear()} VoteChain. All rights reserved.
+        © {new Date().getFullYear()} VoteChain. All rights{" "}
+        <Link to={"/auth/login"}>reserved</Link>.
       </div>
     </footer>
   );
